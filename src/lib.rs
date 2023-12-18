c54fad6f18931e6de611d3825982ec4161dc86a2
Sean Walker <swlkr.rbl@gmail.com>
2023-12-18 10:36:16 -0700

    Add last_modified on the query string for ghetto cache busting

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,15 @@ pub struct StaticFileMeta {
     pub content: &'static str,
     pub content_type: &'static str,
     pub filename: &'static str,
+    pub last_modified: Option<u64>,
 }
 
 impl Display for StaticFileMeta {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        f.write_str(self.filename)
+        match self.last_modified {
+            Some(timestamp) => f.write_fmt(format_args!("{}?v={}", self.filename, timestamp)),
+            None => f.write_str(self.filename),
+        }
     }
 }
 
@@ -28,7 +32,21 @@ type CowStr = Cow<'static, str>;
 
 impl Into<CowStr> for StaticFileMeta {
     fn into(self) -> CowStr {
-        Cow::Borrowed(self.filename)
+        Cow::Owned(self.to_string())
+    }
+}
+
+impl StaticFileMeta {
+    pub fn last_modified(path: &str) -> Option<u64> {
+        let path = format!("{}{}", std::env::var("CARGO_MANIFEST_DIR").unwrap(), path);
+        let metadata = std::fs::metadata(path).unwrap();
+        let modified = metadata.modified().ok()?;
+        let last_modified = modified
+            .duration_since(std::time::SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        Some(last_modified)
     }
 }
 
@@ -48,7 +66,10 @@ mod tests {
         let uri = "/static/test.css";
         let contents = static_files.get(uri).unwrap().content;
         assert_eq!(contents, "/* this is test.css */");
-        assert_eq!(static_files.test.to_string(), "/static/test.css".to_owned());
+        assert_eq!(
+            static_files.test.to_string(),
+            "/static/test.css?v=1702905741".to_owned()
+        );
         assert_eq!(static_files.test.content_type, "text/javascript");
     }
 }
